ce513e719043f2d0da32f8740c3d2814ad7c3a10
Mike <weldonconsulting@gmail.com>
2017-05-01 00:28:24 -0700

    finish index tests

--- test/index-test.ts
+++ test/index-test.ts
@@ -68,9 +68,54 @@ describe('sortedIndex', () => {
       assert.equal(buffer.sortedIndex(3), 2);
       assert.equal(new CircularBuffer(20, [1, 2, 3, 4, 5, 6, 7, 8]).sortedIndex(10), 8);
     });
+  });
 
+  describe('supports classic repetitive item case', () => {
+    let buffer: CircularBuffer<number>
+    beforeEach(() => buffer = new CircularBuffer(10, [0, 0, 0, 1, 1, 2, 2, 3, 3, 7]))
 
+    it('supports repetitive item', () => {
+      assert.equal(buffer.sortedIndex(3), 7);
+      assert.equal(buffer.sortedIndex(5), 9);
+    })
+  })
 
+  describe('mid circular case', () => {
+    let buffer: CircularBuffer<number>
+    beforeEach(() => {
+      buffer = new CircularBuffer(5, [-1, 0, 1, 3, 5]);
+      buffer.push([7, 9]);
+      return buffer;
+    });
 
+    it('simple circular buffer', () => {
+      assert.equal(buffer.sortedIndex(0), 0);
+      assert.equal(buffer.sortedIndex(2), 1);
+      assert.equal(buffer.sortedIndex(4), 2);
+      assert.equal(buffer.sortedIndex(7), 3);
+      assert.equal(buffer.sortedIndex(8), 4);
+      assert.equal(buffer.sortedIndex(10), 4);
+    });
+    it('circular buffer on the pivot', () => {
+      assert.equal(buffer.sortedIndex(4.999), 2);
+      assert.equal(buffer.sortedIndex(5), 2);
+      assert.equal(buffer.sortedIndex(6), 3);
+    });
+    it('returns index of existing item', () => {
+      assert.equal(buffer.sortedIndex(7), 3);
+    });
+  });
+
+  describe('almost sorted data cases (1 item out of place)', () => {
+    let buffer: CircularBuffer<number>
+    beforeEach(() => {
+      buffer = new CircularBuffer(7, [-3, -1, 0, 1, 3, 5, 7]);
+      buffer.push([7, 9, 11, 13, 15, 17]);
+      return buffer;
+    })
+    it('single item out of place', () => {
+      assert.equal(buffer.sortedIndex(0), 0);
+      assert.equal(buffer.sortedIndex(17), 6);
+    })
   })
 })
